5e162911319ac20bac0e24ce0c5d6a624f526e2e
tubo <shitubo@gmail.com>
2022-01-24 15:52:19 +1300

    Update notebook and add runner

---
+++ main.py
@@ -0,0 +1,15 @@
+import re
+import sys
+import pandas as pd
+import numpy as np
+
+from src.parser import parse
+
+filename = sys.argv[1]
+outname = "output/ExamDataParsed_.csv"
+
+data = pd.read_csv(filename)
+parts_sum = data["ce_description"].apply(parse)
+parsed = pd.concat([data, parts_sum.rename("parts_sum")], axis=1)
+result = pd.concat([data, parsed.parts_sum.rename("parts_sum")], axis=1)
+result.to_csv(outname)
